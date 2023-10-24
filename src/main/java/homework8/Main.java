a462cb08c75ef45b3ba05431231825ee59bfab26
sanyo <egp.2277@gmail.com>
2023-10-24 19:23:47 +0300

    First Commit

---
+++ src/main/java/homework8/Main.java
@@ -0,0 +1,11 @@
+package homework8;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car(50, "BMW"); // """ hello """, string joiner
+        Car car2 = new Car(52, "BMW");
+        System.out.println(car1.equals(car2));
+    }
+
+}
+
