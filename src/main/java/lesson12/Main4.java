7ee2db6c35ad20d940ca5178baea89cf01e82bfa
sanyo <egp.2277@gmail.com>
2023-11-07 10:43:10 +0200

    First

---
+++ src/main/java/lesson12/Main4.java
@@ -0,0 +1,11 @@
+package lesson12;
+
+public class Main4 {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+
+        myClass.abstractMethod();
+        myClass.defaultMethod();
+        Interface2.staticMethod();
+    }
+}
